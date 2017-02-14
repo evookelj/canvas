1786060787735b4e873ae7b6fd455279e9b4c570
ChanYvonne <ychan1352@gmail.com>
2017-02-13 21:07:12 -0500

    finished clear and draw something on canvas

--- canvas.js
+++ canvas.js
@@ -1,10 +1,17 @@
 var b = document.getElementById("clear");
-var c = document.getElementById("slate");
-var ctx = c.getContext("2d");
+var canvas = document.getElementById("slate");
+var ctx = canvas.getContext("2d");
 
 b.addEventListener('click',
-		   function(e) {
-		       ctx.clearRect(0,0,500,300);
+		   function() {
+		       ctx.clearRect(0,0,canvas.width,canvas.height);
 		   });
 
-
+canvas.addEventListener('click',
+		   function(event) {
+		       var rect = canvas.getBoundingClientRect();
+		       var x = event.clientX - rect.left;
+		       var y = event.clientY - rect.top;
+		       ctx.fillStyle = '#ffff00';
+		       ctx.fillRect(0, 0, x, y);
+		   });
